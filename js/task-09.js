1514c9313d0df7c4b2f62f9480d62693b10d93e1
Vladymyr Shadura <vs131083@gmail.com>
2022-05-28 22:30:30 +0300

    add homework №9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const refs = {
+  body: document.querySelector("body"),
+  span: document.querySelector(".color"),
+  changeColorBtn: document.querySelector(".change-color"),
+};
+
+console.log(refs.body);
+
+refs.changeColorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+
+  refs.body.style.backgroundColor = randomColor;
+  refs.span.textContent = randomColor;
+});
