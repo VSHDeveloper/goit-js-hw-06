f15cec9c4047a38d858f6137cb287fa8f989b907
Vladymyr Shadura <vs131083@gmail.com>
2022-05-28 22:20:18 +0300

    add homework №9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  if (formElements.email.value === "" || formElements.password.value === "") {
+    return alert("Все поля должны быть заполнены");
+  }
+
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = { email, password };
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
+
+form.addEventListener("submit", onFormSubmit);
